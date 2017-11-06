1fa92b9228a8e4e37788cf8fc8173ceb83968b08
Sam Ruby <rubys@intertwingly.net>
2017-11-06 22:01:25 +0000

    complex cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,9 +27,8 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-	  @line_item = @cart.line_items.build(product: product)
-    
-
+    @line_item = @cart.add_product(product)
+	  #@line_item = @cart.line_items.build(product)
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
@@ -73,6 +72,7 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+    # params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
